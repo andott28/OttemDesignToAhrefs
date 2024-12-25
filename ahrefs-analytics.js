bebe1f905416ad5b942c946a9a4a8ed51e7cfdfa
andott28 <73588440+andott28@users.noreply.github.com>
2024-12-25 14:02:12 +0100

    Create ahrefs-analytics.js

---
+++ ahrefs-analytics.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.defer = true;
+script.src = 'https://analytics.ahrefs.com/analytics.js';
+script.setAttribute('data-key', 'uL2+fAQbrcl8o0Cag40YOw');
+document.getElementsByTagName('head')[0].appendChild(script);
