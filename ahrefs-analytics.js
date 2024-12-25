4a9124852564376b8d7b0fad98629bbe6b8de524
andott28 <73588440+andott28@users.noreply.github.com>
2024-12-25 14:07:35 +0100

    Update ahrefs-analytics.js

--- ahrefs-analytics.js
+++ ahrefs-analytics.js
@@ -1,5 +1,7 @@
-var script = document.createElement('script');
-script.defer = true;
-script.src = 'https://analytics.ahrefs.com/analytics.js';
-script.setAttribute('data-key', 'uL2+fAQbrcl8o0Cag40YOw');
-document.getElementsByTagName('head')[0].appendChild(script);
+<script>
+  var script = document.createElement('script');
+  script.defer = true;
+  script.src = 'https://analytics.ahrefs.com/analytics.js';
+  script.setAttribute('data-key', 'uL2+fAQbrcl8o0Cag40YOw');
+  document.getElementsByTagName('head')[0].appendChild(script);
+</script>
